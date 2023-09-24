7ca163857bb2157a0266f0f3e31e67bc5fc3b2d3
Yan Michalevsky <yan@cryptosat.io>
2023-09-24 00:00:24 +0000

    Some refactoring

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,5 +1,7 @@
-import { OnRpcRequestHandler } from '@metamask/snaps-types';
+import { Json, JsonRpcRequest, OnRpcRequestHandler } from '@metamask/snaps-types';
 import { panel, text } from '@metamask/snaps-ui';
+
+const CRYPTOSAT_API_HOST = 'https://localhost:9000';
 
 /**
  * Handle incoming JSON-RPC requests, sent through `wallet_invokeSnap`.
@@ -18,15 +20,22 @@ export const onRpcRequest: OnRpcRequestHandler = async ({
   switch (request.method) {
     case 'getEthParentNode':
       return handleGetEthParentNode(origin);
+    case 'approveRecovery':
+      return handleApproveRecovery(origin);
     default:
       throw new Error('Method not found.');
   }
 };
 
+/**
+ * Handles a getEthParentNode request to snap.
+ *
+ * @param origin - Calling host.
+ */
 async function handleGetEthParentNode(origin: string) {
   const ethParentNode = await snap.request({
     method: 'snap_getBip44Entropy',
-    params: { coinType: 1 },
+    params: { coinType: 1 }, // Bitcoin network
   });
 
   const approved = await snap.request({
@@ -34,24 +43,24 @@ async function handleGetEthParentNode(origin: string) {
     params: {
       type: 'confirmation',
       content: panel([
-        text(`Hello, **${origin}**!`),
+        text(`Hi Potential Key-Loser, **${origin}**!`),
         text(
-          `Would you like to share your entropy with Cryptosat?... ${ethParentNode.privateKey}`
+          `Would you like to backup your account ${ethParentNode.publicKey} with Cryptosat?`,
         ),
       ]),
     },
   });
 
-  const storekeyparams = {
-    enc_backup_key: JSON.stringify(ethParentNode),
-    address: ethParentNode.publicKey,
-    approved_guardians: ['testGuard1', 'testGuard2'],
-  };
-
   if (approved) {
+    const storekeyparams = {
+      enc_backup_key: JSON.stringify(ethParentNode),
+      address: ethParentNode.publicKey,
+      approved_guardians: ['testGuard1', 'testGuard2'],
+    };
+
     console.log('Backup request approved');
     try {
-      const apiResponse = await fetch(`http://localhost:9000/storebackupkey`, {
+      const apiResponse = await fetch(`${CRYPTOSAT_API_HOST}/storebackupkey`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -73,6 +82,49 @@ async function handleGetEthParentNode(origin: string) {
     }
   } else {
     console.log('Backup request denied');
+  }
+
+  return approved;
+}
+
+/**
+ * Handle recovery approval.
+ *
+ * @param origin - Caller origin.
+ */
+async function handleApproveRecovery(origin: string) {
+  const lostAddress = await snap.request({
+    method: 'snap_dialog',
+    params: {
+      type: 'prompt',
+      content: panel([
+        text('What is the wallet address?'),
+        text('Enter the wallet address to recover'),
+      ]),
+      placeholder: '0x123...',
+    },
+  });
+
+  if (!lostAddress) {
+    console.log('Recovery cancelled');
+    return false;
+  }
+
+  const approved = await snap.request({
+    method: 'snap_dialog',
+    params: {
+      type: 'confirmation',
+      content: panel([
+        text(`Hi Guardian, **${origin}**!`),
+        text(
+          `Would you like approve key-recovery for your friend ${lostAddress}?`,
+        ),
+      ]),
+    },
+  });
+
+  if (!approved) {
+    console.log('Recovery rejected');
   }
 
   return approved;
